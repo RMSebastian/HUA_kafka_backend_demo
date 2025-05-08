1f6df9ab898466de5959e541aa2ab1f0c68a1d14
RMSebastian <105923386+RMSebastian@users.noreply.github.com>
2025-05-08 10:32:31 -0300

    CHANGE: configModule

--- apps/consumer-app/src/consumer-app.module.ts
+++ apps/consumer-app/src/consumer-app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConsumerAppController } from './consumer-app.controller';
 import { ConsumerAppService } from './consumer-app.service';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [ConsumerAppController],
   providers: [ConsumerAppService],
 })
