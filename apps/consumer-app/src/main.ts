9bef4a671f1c2a532ef2fad7912d56964fd3f76c
RMSebastian <105923386+RMSebastian@users.noreply.github.com>
2025-05-08 09:27:03 -0300

    ADD: swagger

--- apps/consumer-app/src/main.ts
+++ apps/consumer-app/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { ConsumerAppModule } from './consumer-app.module';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(ConsumerAppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Consumer App')
+    .setDescription('API that reacts to producer app events')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.port ?? 3001);
 }
 bootstrap();
