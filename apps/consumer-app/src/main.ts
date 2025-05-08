6522fccadd0e0ca10d789fc551244de92c7cbf3b
RMSebastian <105923386+RMSebastian@users.noreply.github.com>
2025-05-08 09:15:50 -0300

    FIX: port

--- apps/consumer-app/src/main.ts
+++ apps/consumer-app/src/main.ts
@@ -3,6 +3,6 @@ import { ConsumerAppModule } from './consumer-app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ConsumerAppModule);
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(process.env.port ?? 3001);
 }
 bootstrap();
