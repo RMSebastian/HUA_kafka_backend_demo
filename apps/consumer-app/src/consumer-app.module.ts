be3ab00b4eacd258b40a9080265c5eb2ec6c46a3
RMSebastian <105923386+RMSebastian@users.noreply.github.com>
2025-05-12 15:49:59 -0300

    ADD: producer-consumer

--- apps/consumer-app/src/consumer-app.module.ts
+++ apps/consumer-app/src/consumer-app.module.ts
@@ -7,21 +7,22 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    // ClientsModule.register([
-    //   {
-    //     name: 'KAFKA_SERVICE',
-    //     transport: Transport.KAFKA,
-    //     options: {
-    //       client: {
-    //         clientId: process.env.KAFKA_CLIENT_ID ?? 'default-client',
-    //         brokers: [process.env.KAFKA_BROKER ?? 'localhost:9092'],
-    //       },
-    //       consumer: {
-    //         groupId: process.env.KAFKA_GROUP_ID ?? 'default-group',
-    //       },
-    //     },
-    //   },
-    // ]),
+    ClientsModule.register([
+      {
+        name: 'KAFKA_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: process.env.KAFKA_CLIENT_ID ?? 'default-client',
+            brokers: [process.env.KAFKA_BROKER ?? 'localhost:9092'],
+          },
+          consumer: {
+            groupId: process.env.KAFKA_GROUP_ID ?? 'default-group',
+          },
+          
+        },
+      },
+    ]),
   ],
   controllers: [ConsumerAppController, ConsumerAppEventHandler],
   providers: [ConsumerAppService],
